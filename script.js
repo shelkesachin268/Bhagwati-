c8565ec0e1a5915d9af36d6271a3c197e988e77e
shelkesachin268 <66778396+shelkesachin268@users.noreply.github.com>
2026-06-13 01:06:40 +0530

    script.js

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
-window.addEventListener("scroll",()=>{
-
-document.querySelectorAll(".card").forEach(card=>{
-
-const pos = card.getBoundingClientRect().top;
-
-if(pos < window.innerHeight-100){
-card.style.opacity="1";
-card.style.transform="translateY(0)";
+const products = [
+{
+ id:1,
+ name:"Shree Datta Swami Jaap Mantra Machine",
+ price:"₹272",
+ images:[
+  "images/product1/1.jpg",
+  "images/product1/2.jpg",
+  "images/product1/3.jpg",
+  "images/product1/4.jpg"
+ ],
+ description:"Bring peace, positivity and spiritual vibrations into your home."
+},
+{
+ id:2,
+ name:"Shree Datta Swami Jaap Mantra Machine",
+ price:"₹272",
+ images:[
+  "images/product2/1.jpg",
+  "images/product2/2.jpg",
+  "images/product2/3.jpg",
+  "images/product2/4.jpg"
+ ],
+ description:"Bring peace, positivity and spiritual vibrations into your home."
 }
-
-});
-
-});
+];
